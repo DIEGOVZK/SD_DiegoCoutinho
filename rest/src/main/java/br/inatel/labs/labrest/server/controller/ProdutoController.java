d3aace603cf0dd9b69296cdbfa8475f4446004af
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-12-02 15:13:01 -0300

    Add CRUD operations to ProdutoController

--- rest/src/main/java/br/inatel/labs/labrest/server/controller/ProdutoController.java
+++ rest/src/main/java/br/inatel/labs/labrest/server/controller/ProdutoController.java
@@ -5,9 +5,13 @@ import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -38,5 +42,22 @@ public class ProdutoController {
         return produto.get();
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto postProduto(@RequestBody Produto produto) {
+        return service.create(produto);
+    }
     
+    @PutMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void putProduto(@RequestBody Produto produto) {
+        service.update(produto);
+    }
+
+    @RequestMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduto(@PathVariable("id") Long produtoId) {
+        service.delete(produtoId);
+    }
+
 }
